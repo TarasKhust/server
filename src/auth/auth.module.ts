517b1a57ddc29227bc099e42d1311c59523d22d7
taras.rushchak <taras.rushchak@dreamscapenetworks.com>
2021-01-19 13:00:42 +0200

    [FE-63] - add auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,9 +6,16 @@ import { LocalStrategy } from './local.strategy';
 import {UserService} from "../user/user.service";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {UserSchema} from "../user/user.schema";
+import {JwtModule} from "@nestjs/jwt";
+import {jwtConstants} from "./constants";
+import {JwtStrategy} from "./jwt.strategy";
 
 @Module({
-  imports: [UserModule, PassportModule, TypeOrmModule.forFeature([UserSchema])],
+  imports: [UserModule, PassportModule, TypeOrmModule.forFeature([UserSchema]), JwtModule.register({
+    secret: jwtConstants.secret,
+    signOptions: { expiresIn: '60s' },
+  }), JwtStrategy],
   providers: [AuthService, LocalStrategy, UserService],
+  exports: [AuthService, JwtStrategy]
 })
 export class AuthModule {}
