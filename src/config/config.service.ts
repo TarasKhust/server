74620e9f886296de95e21f52f9c888078d3300d1
Taras Rushchak | Dreamscape Networks <Taras.Rushchak@dreamscapenetworks.com>
2021-04-04 07:43:29 +0300

    [CF-125][master] - fix

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -40,7 +40,7 @@ class ConfigService {
             password: this.getValue("POSTGRES_PASSWORD"),
             database: this.getValue("POSTGRES_DATABASE"),
 
-            entities: ["**/*.entity{.ts,.js}"],
+            autoLoadEntities: true,
 
             migrationsTableName: "migration",
 
