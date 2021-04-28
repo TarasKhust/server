074d5d4ae42b5d19e610d46cb2472678323701f8
Taras Rushchak | Dreamscape Networks <Taras.Rushchak@dreamscapenetworks.com>
2021-04-28 21:32:43 +0300

    master - add cors

--- src/config/graphql.config.ts
+++ src/config/graphql.config.ts
@@ -1,7 +1,5 @@
 import { ConfigService } from '@nestjs/config';
 
-const whitelist = ['https://clientfront.herokuapp.com/', 'https://clientcrm.herokuapp.com/', 'http://localhost:9000'];
-
 export const graphqlConfig = (configService: ConfigService) => {
 	const development = configService.get('environment') === 'development';
 	return {
@@ -10,21 +8,6 @@ export const graphqlConfig = (configService: ConfigService) => {
 		installSubscriptionHandlers: true,
 		autoSchemaFile: true,
 		context: ({ req }: any) => ({ headers: req.headers }),
-		cors: {
-			origin: function (origin: string, callback: (arg0: Error | null, arg1: boolean | undefined) => any) {
-
-				if (whitelist.indexOf(origin) !== -1) {
-					console.log('allowed cors for:', origin);
-					return callback(null, true);
-				}
-
-					console.log('blocked cors for:', origin);
-					return callback(new Error('Not allowed by CORS'), false);
-
-			},
-			allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-			methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-			credentials: true,
-		},
+		cors: true,
 	};
 };
