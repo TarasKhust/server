8b28b4e7f82723bc3aa04b66b60a0c5184ba4ff4
Taras Rushchak | Dreamscape Networks <Taras.Rushchak@dreamscapenetworks.com>
2021-06-06 12:38:24 +0300

    [CF-160] - fix entity

--- src/order/dto/create-order.input.ts
+++ src/order/dto/create-order.input.ts
@@ -3,11 +3,12 @@ import { Field, InputType, Int } from '@nestjs/graphql';
 import { CreateProductInput } from '../../product/dto/create-product.input';
 import { CreateCustomerInput } from '../../customer/dto/create-customer.input';
 import { CreatePaymentInput } from '../../payment/dto/create-payment.input';
+import { AddOrderProductId } from './add-order-product-id';
 
 @InputType()
 export class CreateOrderInput {
 
-  @Field(() => [CreateProductInput], { nullable: true })
+  @Field(() => [AddOrderProductId], { nullable: true })
   product?: CreateProductInput[];
 
   @Field(() => CreateCustomerInput, { nullable: true })
